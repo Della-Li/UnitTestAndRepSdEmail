4c41921566dd7bb1c6299abac5289afa880bd7aa
Della-Li <lyfyxy66@gmail.com>
2021-01-12 18:19:22 +1100

    'init'

---
+++ README.txt
@@ -0,0 +1,20 @@
+Requirement:
+
+    Implement an automated test that performs the following (steps); 
+	 • Access to woolworths website
+	 • Search with key words 
+	 • Send the testing result to the email
+
+Used Technologies:
+
+	Python, Selenium WebDriver ， DDT， Unittest
+
+Supported Browsers:
+
+        Chrome
+
+Run Test Steps:
+
+    1. Clone this project to a local folder
+    2. Install Python ,download relative chrome driver, pip install other packages
+    3. Run this python script
---
+++ UnitTestAndReportSendEmail.py
@@ -0,0 +1,113 @@
+
+from selenium import webdriver
+from time import sleep
+import csv
+import codecs
+import unittest
+from itertools import islice
+from ddt import ddt, data, file_data, unpack
+
+
+
+
+
+driver = webdriver.Chrome()
+woolworths_url = 'http://www.woolworths.com.au/'
+
+def setUpModule():
+	print('test module start >>>>>>>>>>>>>>>>>>')
+
+def tearDownModule():
+	print('test module end >>>>>>>>>>>>>>>>>>')
+
+
+@ddt
+class TestWoolworths(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.driver = driver
+		cls.base_url = woolworths_url
+	#  the following is for csv file
+		cls.test_data = [ ]
+		with codecs.open('./data_file/woolworths_data.csv','r','utf_8_sig') as f:
+			data = csv.reader(f)
+			for line in islice(data, 1, None):
+				cls.test_data.append(line)
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.driver.quit()
+
+	def woolworths_search(self, search_key):
+		self.driver.get(self.base_url)
+		self.driver.maximize_window() 
+		self.driver.find_element_by_xpath("//input[@id='headerSearch']").click()
+		self.driver.find_element_by_xpath("//input[@id='headerSearch']").send_keys(search_key)
+		self.driver.find_element_by_xpath("//button[@value='Search']").click()
+
+		sleep(4)
+
+	
+
+
+	def test_search_oil(self):
+		self.woolworths_search(self.test_data[0][1])
+
+	def test_search_chocolate(self):
+		self.woolworths_search(self.test_data[1][1])
+
+	def test_search_biscuit(self):
+		self.woolworths_search(self.test_data[2][1])
+
+	# the following is for json file
+	@file_data('./data_file/ddt_data_file.json')
+	def test_search_JsonFile(self, search_key):
+		print('new search through json file：',search_key)	
+		self.woolworths_search(search_key)
+		#self.assertEqual(self.driver.title, search_key+'_WoolWorthsSearch')
+
+	# the following is for yaml file
+	@file_data('./data_file/ddt_data_file.yaml')
+	def test_search_YamlFile(self, case):
+		search_key = case[0]['search_key']
+		print('new search through yaml file：',search_key)
+		self.woolworths_search(search_key)
+		#self.assertEqual(self.driver.title, search_key+'_WoolWorthsSearch')
+
+
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
+
+
+'''
+
+#邮箱密码：Lyfyxy66 
+#连接邮箱服务器
+def send_mail(report):
+	yag = yagmail.SMTP(user='senderlyf@126.com', 
+		password='KOENVNFCQTFVLQCN',
+		host='smtp.126.com')
+	subject = 'topic, autumation testing report'
+	contents = 'For contents ,please check the attachment'
+	yag.send(['receiverlyf@126.com','senderlyf@126.com'], subject, contents, report)
+	print('email has sent out')
+
+#邮件正文
+if __name__ == '__main__':
+	test_dir = './test_case'
+	suit = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py')
+
+	now_time = time.strftime('%Y-%m-%d %H_%M_%S')
+	print(now_time)
+	html_report = './test_report/' + now_time + 'result.html'
+	fp = open(html_report, 'wb')
+
+	runner = HTMLTestRunner(stream=fp, title='Search testing report', description='testing environment： Windows 10, Browser Chrome')
+	runner.run(suit)
+	fp.close()
+
+	send_mail(html_report)''
+
+'''
